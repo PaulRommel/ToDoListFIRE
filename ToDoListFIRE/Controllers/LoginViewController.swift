4fda2c8c8005822d75ce20dd8d731ba4062ebea1
Paul <paulrommel87@gmail.com>
2022-04-04 23:30:45 +0300

    3 Creating tasks screen

---
+++ ToDoListFIRE/Controllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  ToDoListFIRE
+//
+//  Created by Павел Попов on 04.04.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+    }
+    
+}
---
+++ ToDoListFIRE/Controllers/TasksViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TasksViewController.swift
+//  ToDoListFIRE
+//
+//  Created by Павел Попов on 04.04.2022.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+}
