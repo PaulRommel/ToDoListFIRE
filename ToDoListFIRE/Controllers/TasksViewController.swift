ce4e6ad46d9bd9a0e05e20408ccfea118027f110
Paul <paulrommel87@gmail.com>
2022-04-10 21:43:13 +0300

    7 Task form. JSON Data

--- ToDoListFIRE/Controllers/TasksViewController.swift
+++ ToDoListFIRE/Controllers/TasksViewController.swift
@@ -25,6 +25,22 @@ class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return 5
     }
     @IBAction func addTapped(_ sender: UIBarButtonItem) {
+        
+        let alertController = UIAlertController(title: "New Task", message: "Add new task", preferredStyle: .alert)
+        alertController.addTextField()
+        let save = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let textField = alertController.textFields?.first, textField.text != "" else { return }
+            
+            //let task
+            //task Ref
+            //
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        alertController.addAction(save)
+        alertController.addAction(cancel)
+        
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func singOuyTapped(_ sender: UIBarButtonItem) {
